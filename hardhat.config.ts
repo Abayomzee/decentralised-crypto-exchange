48c7477a8141bffaca5760c04f3f7df5d8dd2986
= <=>
2022-10-28 16:04:18 +0100

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
