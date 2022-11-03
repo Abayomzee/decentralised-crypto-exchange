0b49e9c22314b640c007660647327f7fb7673bdd
= <=>
2022-11-03 08:39:10 +0100

    2. Smoke test

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import 'hardhat/console.sol';
+
+contract Token {
+    string public name = 'My Token';
+
+    constructor() {
+        
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {},
+  },
 };
 
 export default config;
