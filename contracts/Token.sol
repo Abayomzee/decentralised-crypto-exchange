3f60ff187423211b3cddf94d8c654eb17ef9a82c
= <=>
2022-11-04 18:18:51 +0100

    3. Testing Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,8 +5,11 @@ import 'hardhat/console.sol';
 
 contract Token {
     string public name = 'My Token';
+    string public symbol = 'DAPP';
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000  * (10**decimals);
 
-    constructor() {
+    constructor() { 
         
     }
 }
---
+++ test/Token.js
@@ -0,0 +1,40 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+describe("Token", () => {
+  const tokenName = "My Token";
+  const tokenSymbol = "DAPP";
+  const tokenDecimals = 18;
+  // const tokenTotalSupply = "1000000000000000000000000";
+  const tokenTotalSupply = tokens("1000000");
+  let token;
+
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy();
+  });
+
+  it("has a correct name", async () => {
+    const name = await token.name();
+    expect(name).to.equal(tokenName);
+  });
+
+  it("has a correct symbol", async () => {
+    const symbol = await token.symbol();
+    expect(symbol).to.equal(tokenSymbol);
+  });
+  it("has a correct decimals", async () => {
+    const decimals = await token.decimals();
+    expect(decimals).to.equal(tokenDecimals);
+  });
+  it("has a correct totalSupply", async () => {
+    const totalSupply = await token.totalSupply();
+    expect(totalSupply).to.equal(tokenTotalSupply);
+  });
+});
