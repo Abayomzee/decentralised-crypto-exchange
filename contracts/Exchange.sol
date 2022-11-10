7d82da6130f35431405509226b9b3908907ece95
= <=>
2022-11-11 00:29:51 +0100

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,6 +11,7 @@ contract Exchange {
 
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent){
         feeAccount = _feeAccount;
@@ -22,12 +23,26 @@ contract Exchange {
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
-        // Emit a deposit evebt
+        // Emit a deposit event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
+
+    // Withdraw Token
+    function withdrawToken(address _token, uint256 _amount) public {
+        require(tokens[_token][msg.sender] >= _amount);
+
+        // Transfer tokens to user
+        Token(_token).transfer(msg.sender, _amount);
+        
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        // Emit an event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
    
-//    Balance oo
-function balanceOf(address _token, address _user) public view returns (uint256){
-    return tokens[_token][_user];
-}
+    //    Balance of
+    function balanceOf(address _token, address _user) public view returns (uint256){
+        return tokens[_token][_user];
+    }
 }
