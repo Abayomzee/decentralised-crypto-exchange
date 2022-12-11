178ef1fe88d6551ac8e846b90af9482be18f0757
= <=>
2022-12-11 18:24:05 +0100

    17. State management pt 2

--- src/Store/setup.js
+++ src/Store/setup.js
@@ -4,51 +4,113 @@ import { devtools } from "zustand/middleware";
 //
 import { addresses } from "Utils/Constants";
 import TokenAbi from "Abis/Token.json";
-import { c } from "Utils/Helpers";
+import ExchangeAbi from "Abis/Exchange.json";
 
 const useSetup = create(
   devtools((set, get) => ({
-    provider: null,
-    chainId: "",
-    account: "",
-    token: null,
-    symbol: "",
+    provider: {
+      connection: null,
+      chainId: "",
+      account: "",
+      balance: "",
+    },
+    tokens: {
+      contracts: [],
+      symbols: [],
+    },
+    exchange: null,
 
     // Methods
     loadProvider: () => {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = { ...get().provider };
+      const connection = new ethers.providers.Web3Provider(window.ethereum);
+      provider.connection = connection;
       set({ provider }, false, "Provider_Loaded");
-      return provider;
+      return connection;
     },
     loadNetwork: async () => {
+      const provider = { ...get().provider };
       const { chainId } = await get().loadProvider().getNetwork();
-      c({ chainId });
-      set({ chainId }, false, "Network_Loaded");
+      provider.chainId = chainId;
+      set({ provider }, false, "Network_Loaded");
       return chainId;
     },
     loadAccount: async () => {
+      const provider = { ...get().provider };
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
       const account = ethers.utils.getAddress(accounts[0]);
-      c({ account });
-      set({ account }, false, "Account_Loaded");
+      provider.account = account;
+      set({ provider }, false, "Account_Loaded");
+
       return account;
     },
+    loadBalance: async () => {
+      const provider = { ...get().provider };
+
+      let connection = await get().provider.connection;
+      let balance = await connection.getBalance(provider.account);
+      balance = ethers.utils.formatEther(balance);
+      provider.balance = balance;
+
+      set({ provider }, false, "Balance_Loaded");
+    },
+
     loadToken: async () => {
+      const tokens = { ...get().tokens };
+
       let token, symbol;
 
-      token = new ethers.Contract(addresses.Dapp, TokenAbi, get().provider);
+      // Load Token 1
+      token = new ethers.Contract(
+        addresses.Dapp,
+        TokenAbi,
+        get().provider.connection
+      );
       symbol = await token.symbol();
-      c({ token, symbol });
-      set({ token, symbol }, false, "Token_Loaded");
+      tokens.contracts[0] = token;
+      tokens.symbols[0] = symbol;
+      set({ tokens }, false, "Token_1_Loaded");
+
+      // Load Token 2
+      token = new ethers.Contract(
+        addresses.mETH,
+        TokenAbi,
+        get().provider.connection
+      );
+      symbol = await token.symbol();
+      tokens.contracts[1] = token;
+      tokens.symbols[1] = symbol;
+      set({ tokens }, false, "Token_2_Loaded");
+
       return token;
     },
+    loadExchange: async () => {
+      let exchange;
+
+      exchange = new ethers.Contract(
+        addresses.exchange,
+        ExchangeAbi,
+        get().provider.connection
+      );
+      set({ exchange }, false, "Exchange_Loaded");
+
+      return exchange;
+    },
     initSetUp: async () => {
-      await get().loadAccount();
+      // Connect Ethers to blockchain
       await get().loadProvider();
+      // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
       await get().loadNetwork();
+      // Fetch current account from metamask
+      await get().loadAccount();
+      // Fetch balance of current account from metamask
+      await get().loadBalance();
+      // Load token smart contracts
       await get().loadToken();
+      // Load exchange smart contracts
+      await get().loadExchange();
     },
   }))
 );
