93027ae48c89ce7e23f843cf4f583f35bb3b0694
= <=>
2022-12-25 00:44:39 +0100

    19. Select Markets

--- src/Components/Molecules/Markets/style.tsx
+++ src/Components/Molecules/Markets/style.tsx
@@ -3,6 +3,5 @@ import { motion } from "framer-motion";
 
 export const MarketSelector = styled(motion.div)`
   padding: 6rem 0 2rem;
-  margin-top: 5rem;
   border-bottom: 0.1rem solid var(--color-7);
 `;
